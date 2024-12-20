c951ee555e9f7034a1618cc161c9d95835acbf81
dy3no <165803560+dy3no@users.noreply.github.com>
2024-12-20 15:33:50 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "7688784612:AAG2i-qcDpxY3GNe7hdipfWCXToXW42kO2g")
     FSUB = getenv("FSUB", "TheAllLink")
     CHID = int(getenv("CHID", "-1002201654960"))
-    SUDO = list(map(int, getenv("6987158459").split()))
+    SUDO = list(map(int, getenv("6987158459").split())
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://akashrabha2005:781120@cluster0.pv6yd2f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
